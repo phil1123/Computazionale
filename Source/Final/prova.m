ecaeab2a0548e86423af3cbc1192555bb844c8d1
phil1123 <filippo.quarenghi@gmail.com>
2018-07-18 00:41:57 +0200

    prima versione

--- Source/Final/prova.m
+++ Source/Final/prova.m
@@ -3,9 +3,7 @@ clc; clear;
 
 h = 1; % hbar 
 
-% N = 10;
- N = 2048;
-% N = 8;
+N = 100;
 
 xLim = 50;
 b = 5;
@@ -13,7 +11,7 @@ b = 5;
 box = [-xLim xLim];
 x = linspace(box(1),box(2),N)';
 nb = find(x>b,1);
-mb = find(x<b,1);
+mb = find(x>-b,1);
 
 dx = x(2)-x(1); 
 
@@ -37,6 +35,7 @@ K0 = sqrt(2*E0);
 K1 = sqrt(2*E1);
 
 analyticT = transmission(E0,V0,2*b); %Coefficiente teorico
+
 %% Plot delle funzioni d'onda
 J = 3;
 
@@ -58,68 +57,30 @@ line([-b -b],[-0.2 0.2])
 
 grid on
 
-%% Calcolo della differenza di fase prima e dopo la barriera
-
-MS = N-nb;
-X = x(1:MS);
-
-fase = zeros(N,1);
-
-for i=1:N
-    Y1 = M1(1:MS,i);
-    Y2 = M1(nb:nb+MS-1,i);
-    fase(i) = phdiffmeasure(Y1,Y2);
-end
-
-figure
-plot(1:N,fase)
-grid on
-
-%% Coefficiente di trasmissione (sbagliato)
-
-T = zeros(N,1);
-for i = 1:N
-    T(i) = abs( M1(nb,i) - M0(nb,i) ) ./ abs( M1(1,i) - M0(1,i) );
-end
-
-figure
-subplot(1,2,1)
-plot(E0/V0,T,'-r');
-
-xlabel('E/V0')
-ylabel('Transmission coefficient')
-title('Experimental')
-
-subplot(1,2,2)
-plot(E0/V0,analyticT,'-b');
-
-xlabel('E/V0')
-ylabel('Transmission coefficient')
-title('Analytical')
-
-%% Calcolo coefficiente di trasmissione come 1 - R
+%%
 
 A1 = (M1+M0).^2;
+A1 = A1(1:mb,1:N);
+A1 = sum(A1);
+
 A2 = M1.^2;
+A2 = A2(1:mb,1:N);
+A2 = sum(A2);
+
 A3 = M0.^2;
+A3 = A3(1:mb,1:N);
+A3 = sum(A3);
 
-Kn = (K1-K0)*x';
-Km = (K1+K0)*x';
+B1 = 2*cos((K1-K0)*x')';
+B1 = B1(1:mb,1:N);
+B1 = sum(B1);
 
-for i=1:mb
-    f1 = A2 - A3 - A1 - 2*cos(Kn) * 10000;
-end
+B2 = 2*cos((K1+K0)*x')';
+B2 = B2(1:mb,1:N);
+B2 = sum(B2);
 
-I1 = sum(f1);
-
-for i=1:mb
-    f2 = 2*cos(Km);
-end
-
-I2 = sum(f2);
-
-R = I1./I2;
-
+R = (A1-A2-A3-B1)./B2;
+% R = A1-A2-A3-B1;
 T = 1-R;
 
 figure
